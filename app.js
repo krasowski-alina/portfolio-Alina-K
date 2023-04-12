8618481f10d5ff95deeed55074a4fdbecc59219c
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-04-12 11:46:18 -0400

    added slider with certificates

--- app.js
+++ app.js
@@ -10,4 +10,28 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
-
+// Slider
+const btns = document.querySelectorAll(".btn-slider");
+let i = 0;
+const certificates = [
+                      "certificates/JavaScript.png",
+                      "certificates/gsap.png",
+                      "certificates/bootstrap.png",
+                      "certificates/Introduction to Web Development.png",
+                      ];
+btns.forEach(item => {
+  item.addEventListener("click", (event)=> {
+    let btn = event.target.id;
+    document.querySelector("#certificate").src = certificates[i]
+    if(btn === "next"){
+      i++;
+      if( i > certificates.length - 1) {
+        i=0
+      }
+    }
+    else i--;
+      if( i < 0){
+        i = certificates.length - 1;
+    }
+  })
+})
